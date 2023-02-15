43127bafd0e41abb0e1df8e9ff2516cbb51db15f
edmar.james <edmarjamesbautista@gmail.com>
2023-02-15 18:34:27 +0800

    Add comments on s2 JS file

--- s2-traversing-elements/index.js
+++ s2-traversing-elements/index.js
@@ -9,9 +9,14 @@
     Syntax:
         node.parentNode;
 */
-const heading = document.getElementById('heading');
-console.log("This is the parentNode of heading \n");
-console.log(heading.parentNode);
+function getParentNode() {
+    // get the element with the id of 'heading'
+    const heading = document.getElementById('heading');
+    console.log("This is the parentNode of heading \n");
+    // logs the 'parentNode' of 'heading' element
+    console.log(heading.parentNode);
+};
+getParentNode();
 
 // 2. firstChild, firstElementChild, lastChild, lastElementChild, childNodes and children
 // Getting child elements of a node
@@ -27,14 +32,20 @@ console.log(heading.parentNode);
         parentElement.firstChild;
 
 */
+// get the element with the id of 'prog_field'
 const field = document.querySelector('#prog_field');
-console.log("This is the firstChild of UL");
-console.log(field.firstChild.nodeName);
+
+function getFirstChild() {    
+    console.log("This is the firstChild of UL");
+    // logs the 'firstChild' of the 'field' element
+    console.log(field.firstChild.nodeName);
+};
+getFirstChild();
 
 /* 
     The console window show #text because a text node is inserted to maintain the whitespace between the opening <ul> and <li> tags. This whitespace creates a #text node
 
-    NOTE: any whitespace such as a single space, multiple spaces, returns and tabs will create a #text node. To remove the #text node, you can remove the whitespaces by converting it as a single line.
+    NOTE: any whitespace such as a single space, multiple spaces, returns and tabs will create a #text node. To remove the #text node, you can remove the white spaces by converting it as a single line.
 */
 
 /* 
@@ -44,8 +55,12 @@ console.log(field.firstChild.nodeName);
     Syntax:
         parentElement.firstElementChild;
 */
-console.log("This is the firstElementChild of UL");
-console.log(field.firstElementChild);
+function getFirstElementChild() {
+    console.log("This is the firstElementChild of UL");
+    // logs the 'firstElementChild' of 'field' element
+    console.log(field.firstElementChild);
+};
+getFirstElementChild();
 
 /* 
     lastChild
@@ -55,8 +70,12 @@ console.log(field.firstElementChild);
     Syntax:
         parentElement.lastChild;
 */
-console.log("This is the lastChild of UL");
-console.log(field.lastChild);
+function getLastChild() {
+    console.log("This is the lastChild of UL");
+    // logs the 'lastChild' of 'field' element
+    console.log(field.lastChild);
+};
+getLastChild();
 
 /* 
     lastElementChild
@@ -65,8 +84,12 @@ console.log(field.lastChild);
     Syntax:
         parentElement.lastElementChild;
 */
-console.log("This is the lastElementChild of UL");
-console.log(field.lastElementChild);
+function getLastElementChild() {
+    console.log("This is the lastElementChild of UL");
+    // logs the 'lastElementChild' of 'field' element
+    console.log(field.lastElementChild);
+};
+getLastElementChild();
 
 /* 
     childNodes
@@ -75,8 +98,12 @@ console.log(field.lastElementChild);
     Syntax:
         parentElement.childNodes;
 */
-console.log("This are all the child nodes of UL with ANY node type");
-console.log(field.childNodes);
+function getChildNodes() {
+    console.log("This are all the child nodes of UL with ANY node type");
+    // logs all 'childNodes' of 'field' element regardless of node type
+    console.log(field.childNodes);
+};
+getChildNodes();
 
 /* 
     children
@@ -85,8 +112,12 @@ console.log(field.childNodes);
     Syntax:
         parentElement.children;
 */
-console.log("This are all the child nodes of UL with ELEMENT node type");
-console.log(field.children);
+function getChildren() {
+    console.log("This are all the child nodes of UL with ELEMENT node type");
+    // logs all 'children' of 'field' with 'element' node type
+    console.log(field.children);
+};
+getChildren();
 
 
 // 3. nextElementSibling, previousElementSibling
@@ -99,20 +130,33 @@ console.log(field.children);
     Syntax:
         currentNode.nextElementSibling;
 */
+// get the element with the id of 'current'
 const current = document.querySelector('#current');
-console.log('This is the next sibling element of the element with id="current"');
-console.log(current.nextElementSibling);
+
+function getNextElementSibling() {
+    console.log('This is the next sibling element of the element with id="current"');
+    // logs the 'nextElementSibling' of 'current' element
+    console.log(current.nextElementSibling);
+};
+getNextElementSibling();
 
 /* 
     To get all the next siblings of an element you can use the following code
 */
-let nextSibling = current.nextElementSibling;
+function getAllNextElementSibling() {
+    // assign the 'nextElementSibling' of 'current' element to 'nextSibling' variable
+    let nextSibling = current.nextElementSibling;
 
-console.log('These are all the next siblings of the element with id="current"');
-while (nextSibling) {
-    console.log(nextSibling);
-    nextSibling = nextSibling.nextElementSibling;
-}
+    console.log('These are all the next siblings of the element with id="current"');
+    // checks if 'nextSibling' has a value
+    while (nextSibling) {
+        // logs 'nextSibling'
+        console.log(nextSibling);
+        // update the value of 'nextSibling' to the 'nextElementSibling'
+        nextSibling = nextSibling.nextElementSibling;
+    }
+};
+getAllNextElementSibling();
 
 /*  
     previousElementSibling
@@ -122,9 +166,14 @@ while (nextSibling) {
     Syntax:
         currentNode.previousElementSibling;
 */
-const loveToTry = document.querySelector('#would-love-to-try');
-console.log('This is the previous sibling element of the element with id="would-love-to-try"');
-console.log(loveToTry.previousElementSibling);
+function getPreviousElementSibling() {
+    // get the element with the id of 'would-love-to-try'
+    const loveToTry = document.querySelector('#would-love-to-try');
+    console.log('This is the previous sibling element of the element with id="would-love-to-try"');
+    // logs the 'previousElementSibling' of 'loveToTry' element
+    console.log(loveToTry.previousElementSibling);
+};
+getPreviousElementSibling();
 
 /* 
     To get all siblings of an element, we'll use this logic
@@ -149,16 +198,22 @@ function getAllSiblings(e) {
 
     // get all the siblings
     while (sibling) {
+        // checks if the type of node is 'element' and it is not equal to the current target
         if (sibling.nodeType === 1 && sibling !== e) {
+            // push it to the siblings array
             siblings.push(sibling);
         }
+        // update the value of sibling to the 'nextSibling'
         sibling = sibling.nextSibling;
     }
 
     return siblings;
 }
 
+// assign the result of 'getAllSiblings' function to 'niceToLearn' variable
 let niceToLearn = getAllSiblings(document.querySelector('#nice-to-learn'));
+// assign the result of mapping through the 'niceToLearn' variable to 'siblingText' variable
 let siblingText = niceToLearn.map(s => s.innerHTML);
 console.log('These are all the sibling of the element with id="nice-to-learn"');
+// logs the 'siblingText' array
 console.log(siblingText);
