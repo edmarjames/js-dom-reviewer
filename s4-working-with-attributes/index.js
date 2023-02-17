2f35e7e3c4d62b0e4e7229852ee3e70ef3b10b4d
edmar.james <edmarjamesbautista@gmail.com>
2023-02-17 17:37:49 +0800

    Arrange codes into functions and add comments on s4 HTML file

--- s4-working-with-attributes/index.js
+++ s4-working-with-attributes/index.js
@@ -10,11 +10,14 @@
 // select the element with the id of heading
 let text = document.querySelector('#heading');
 
-// loop through the attributes and print its name and value
-console.log('\n\t\tThese are the attributes of the header\n\n'.toUpperCase());
-for (let attr of text.attributes) {
-    console.log(`${attr.name} ${attr.value}`);
-}
+function getAllAttributes() {
+    // loop through the attributes and print its name and value
+    console.log('\n\t\tThese are the attributes of the header\n\n'.toUpperCase());
+    for (let attr of text.attributes) {
+        console.log(`${attr.name} ${attr.value}`);
+    }
+};
+getAllAttributes();
 
 
 // 2. Attribute-property synchronization
@@ -22,17 +25,20 @@ for (let attr of text.attributes) {
 /* 
     - when a standard attribute changes, the corresponding property is auto-updated with some exceptions and vice versa
 */
-console.log('\n\t\tAttribute-property synchronization\n\n'.toUpperCase());
+function attributePropertySynchronization() {
+    console.log('\n\t\tAttribute-property synchronization\n\n'.toUpperCase());
 
-// to set an attribute value you can use setAttribute
-text.setAttribute('class', 'first-header');
-// to get the value of an attribute you can access it by using dot notation - element.attribute
-console.log(text.className);
+    // to set an attribute value you can use setAttribute
+    text.setAttribute('class', 'first-header');
+    // to get the value of an attribute you can access it by using dot notation - element.attribute
+    console.log(text.className);
 
-// You can also set an attribute value by accessing it and assigning the value directly
-text.className = "second-header";
-// You can also get the value of an attribute by using getAttribute
-console.log(text.getAttribute('class'));
+    // You can also set an attribute value by accessing it and assigning the value directly
+    text.className = "second-header";
+    // You can also get the value of an attribute by using getAttribute
+    console.log(text.getAttribute('class'));
+};
+attributePropertySynchronization();
 
 
 // 3. DOM properties are typed
@@ -40,81 +46,90 @@ console.log(text.getAttribute('class'));
 /* 
     - the value of an attribute is always a string. However, when the attribute is converted to the property of a DOM object, the property value can be a string, a boolean, an object, etc.
 */
-// select the element with the id of submitBtn
-const btn = document.querySelector('#submitBtn');
-// select the element with the id of container
-const container = document.querySelector('#container');
+function domPropertiesAreTyped() {
+    // select the element with the id of 'submitBtn'
+    const btn = document.querySelector('#submitBtn');
+    // select the element with the id of 'container'
+    const container = document.querySelector('#container');
 
-// add an event listener to the submitBtn
-btn.addEventListener('click', () => {
-    // get the elements with the name of 'theme'
-    const themes = document.getElementsByName('theme');
+    // add an event listener to the submitBtn
+    btn.addEventListener('click', () => {
+        // get the elements with the name of 'theme'
+        const themes = document.getElementsByName('theme');
 
-    // loop through the themes
-    themes.forEach(color => {
-        // if an element is checked, then get its value and set it as the background color for container
-        if (color.checked) {
-            console.log('\n\t\tThe checked attribute is converted to a boolean value\n\n'.toUpperCase());
-            console.log(color.checked);
+        // loop through the themes
+        themes.forEach(color => {
+            // if an element is checked, then get its value and set it as the background color for container
+            if (color.checked) {
+                console.log('\n\t\tThe checked attribute is converted to a boolean value\n\n'.toUpperCase());
+                // this outputs a boolean value
+                console.log(color.checked);
 
-            container.style.backgroundColor = color.value;
-            const header = document.querySelector('#heading');
-            const labels = document.getElementsByTagName('label');
-            const h3 = document.querySelector('.footer h3');
-            const icons = document.getElementsByTagName('i');
-            const small = document.getElementsByTagName('small');
-            const copyright = document.querySelector('.copyright');
-            const hr = document.getElementsByTagName('hr');
-            const h2 = document.querySelector('h2');
+                // set the background color of container to the selected radio button value
+                container.style.backgroundColor = color.value;
+                // get all the text labels
+                const header = document.querySelector('#heading');
+                const labels = document.getElementsByTagName('label');
+                const h3 = document.querySelector('.footer h3');
+                const icons = document.getElementsByTagName('i');
+                const small = document.getElementsByTagName('small');
+                const copyright = document.querySelector('.copyright');
+                const hr = document.getElementsByTagName('hr');
+                const h2 = document.querySelector('h2');
 
-            function setToWhite(header, labels, h3, icons, small, copyright, hr, h2) {
-                header.style.color = "white";
-                h3.style.color = "white";
-                copyright.style.color = "white";
-                h2.style.color = "white";
-                for (let h of hr) {
-                    h.className = "hr-white";
+                // set all text labels to white
+                function setToWhite(header, labels, h3, icons, small, copyright, hr, h2) {
+                    header.style.color = "white";
+                    h3.style.color = "white";
+                    copyright.style.color = "white";
+                    h2.style.color = "white";
+                    for (let h of hr) {
+                        h.className = "hr-white";
+                    };
+                    for (let l of labels) {
+                        l.style.color = "white";
+                    };
+                    for (let i of icons) {
+                        i.style.color = "white";
+                    };
+                    for (let s of small) {
+                        s.style.color = "white";
+                    };
+                }
+
+                // set all text labels to black
+                function setToBlack(header, labels, h3, icons, small, copyright, hr, h2) {
+                    header.style.color = "black";
+                    h3.style.color = "black";
+                    copyright.style.color = "black";
+                    h2.style.color = "black";
+                    for (let h of hr) {
+                        h.className = "hr";
+                    }; 
+                    for (let l of labels) {
+                        l.style.color = "black";
+                    }
+                    for (let i of icons) {
+                        i.style.color = "black";
+                    }
+                    for (let s of small) {
+                        s.style.color = "black";
+                    }
                 };
-                for (let l of labels) {
-                    l.style.color = "white";
-                };
-                for (let i of icons) {
-                    i.style.color = "white";
-                };
-                for (let s of small) {
-                    s.style.color = "white";
-                };
+
+                // if the color selected is dark, set the color of heading and labels to white
+                if (color.value == '#131313') {
+                    setToWhite(header, labels, h3, icons, small, copyright, hr, h2);
+                
+                // otherwise, set it to black
+                } else {
+                    setToBlack(header, labels, h3, icons, small, copyright, hr, h2);
+                }
             }
-
-            function setToBlack(header, labels, h3, icons, small, copyright, hr, h2) {
-                header.style.color = "black";
-                h3.style.color = "black";
-                copyright.style.color = "black";
-                h2.style.color = "black";
-                for (let h of hr) {
-                    h.className = "hr";
-                }; 
-                for (let l of labels) {
-                    l.style.color = "black";
-                }
-                for (let i of icons) {
-                    i.style.color = "black";
-                }
-                for (let s of small) {
-                    s.style.color = "black";
-                }
-            };
-            // if the color selected is dark, set the color of heading and labels to white
-            if (color.value == '#131313') {
-                setToWhite(header, labels, h3, icons, small, copyright, hr, h2);
-            
-            // otherwise, set it to black
-            } else {
-                setToBlack(header, labels, h3, icons, small, copyright, hr, h2);
-            }
-        }
+        });
     });
-});
+};
+domPropertiesAreTyped();
 
 
 // 4. The data-* attributes
@@ -127,8 +142,12 @@ btn.addEventListener('click', () => {
 */
 // get the element with the class of 'theme-container'
 const themeContainer = document.querySelector('.theme-container');
-console.log('\n\t\tHere are the data-* attributes of the themeContainer\n\n'.toUpperCase());
-console.log(themeContainer.dataset);
+
+function getDataset() {
+    console.log('\n\t\tHere are the data-* attributes of the themeContainer\n\n'.toUpperCase());
+    console.log(themeContainer.dataset);
+};
+getDataset();
 
 
 // 5. setAttribute
@@ -145,12 +164,15 @@ console.log(themeContainer.dataset);
         - 'name' specifies the attribute name whose value is set. It's automatically converted to lowercase if you call the setAttribute() on an HTML element.
         - 'value' specifies the value to assign to the attribute. It's automatically converted to a string if you pass a non-string value to the method.
 */
-// get the firstElementChild of themeContainer
-const firstLabel = themeContainer.firstElementChild;
-// get the child of the firstLabel which is the input element
-const child = firstLabel.firstElementChild;
-// add a disabled attribute to the input element
-child.setAttribute('disabled', '');
+function useSetAttribute() {
+    // get the 'firstElementChild' of 'themeContainer'
+    const firstLabel = themeContainer.firstElementChild;
+    // get the child of the firstLabel which is the input element
+    const child = firstLabel.firstElementChild;
+    // add a disabled attribute to the input element
+    child.setAttribute('disabled', '');
+};
+useSetAttribute();
 
 
 // 6. getAttribute
@@ -165,10 +187,13 @@ child.setAttribute('disabled', '');
     Parameters:
         - 'name' which is the name of the attribute from which you want to return the value.
 */
-console.log('\n\t\tThis is the value of the "data-value" attribute of themeContainer\n\n'.toUpperCase());
-console.log(themeContainer.getAttribute('data-value'));
-console.log('\t\tgetAttribute will return null if the attribute is not existing on the element'.toUpperCase());
-console.log(themeContainer.getAttribute('title'));
+function useGetAttribute() {
+    console.log('\n\t\tThis is the value of the "data-value" attribute of themeContainer\n\n'.toUpperCase());
+    console.log(themeContainer.getAttribute('data-value'));
+    console.log('\t\tgetAttribute will return null if the attribute is not existing on the element'.toUpperCase());
+    console.log(themeContainer.getAttribute('title'));
+};
+useGetAttribute();
 
 // 7. removeAttribute
 
@@ -183,10 +208,14 @@ console.log(themeContainer.getAttribute('title'));
     Parameters:
         - 'name' which is the name of the attribute that you want to remove. If the attribute does not exist, the method will NOT raise an error.
 */
-themeContainer.removeAttribute('data-status');
-console.log('\n\t\tThe data-status attribute is now removed from the themeContainer\n\n'.toUpperCase());
-// returns the innerHTML of the whole container to show that the data-status attribute of themeContainer is already removed
-console.log(container.innerHTML);
+function useRemoveAttribute() {
+    // use 'removeAttribute' to remove an attribute
+    themeContainer.removeAttribute('data-status');
+    console.log('\n\t\tThe data-status attribute is now removed from the themeContainer\n\n'.toUpperCase());
+    // returns the innerHTML of the whole container to show that the data-status attribute of themeContainer is already removed
+    console.log(container.innerHTML);
+};
+useRemoveAttribute();
 
 // 8. hasAttribute
 
@@ -200,12 +229,22 @@ console.log(container.innerHTML);
     Parameters:
         - 'name' which is the name of the attribute that you want to check
 */
-console.log('\n\t\tThe themeContainer has a class attribute\n\n'.toUpperCase());
-// returns true
-console.log(themeContainer.hasAttribute('class'));
+function useHasAttribute() {
+    console.log('\n\t\tThe themeContainer has a class attribute\n\n'.toUpperCase());
+    // returns true
+    console.log(themeContainer.hasAttribute('class'));
+};
+useHasAttribute();
+
 
 // footer
-const footer = document.querySelector('.footer');
-const horizontalRule = document.createElement('hr');
+function footerSection() {
+    // select the element with the class of 'footer'
+    const footer = document.querySelector('.footer');
+    // create a 'hr' element
+    const horizontalRule = document.createElement('hr');
 
-container.insertBefore(horizontalRule, footer);
+    // insert the 'hr' before the 'footer'
+    container.insertBefore(horizontalRule, footer);
+};
+footerSection();
